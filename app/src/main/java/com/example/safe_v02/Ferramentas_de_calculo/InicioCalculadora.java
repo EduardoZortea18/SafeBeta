807937825f02f8775c024e469a5ad6fe1f729b3a
Mark1 <joao.b19@aluno.ifsc.edu.br>
2020-03-10 09:05:53 -0300

    Tela de tutorial pronta. 2.0

--- app/src/main/java/com/example/safe_v02/Ferramentas_de_calculo/InicioCalculadora.java
+++ app/src/main/java/com/example/safe_v02/Ferramentas_de_calculo/InicioCalculadora.java
@@ -14,7 +14,7 @@ import com.example.safe_v02.R;
 
 public class InicioCalculadora extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    Spinner Operacao;
+    Spinner spinner_operacoes,spinner_materias;
     Button btnProximo;
     Toolbar toolbar;
 
@@ -31,30 +31,56 @@ public class InicioCalculadora extends AppCompatActivity implements AdapterView.
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setTitle("Ferramentas de cálculo");
 
-        //Seta o spinner e a sua lista de itens
-        Operacao = (Spinner) findViewById(R.id.Operacao);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.operacoes_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Operacao.setAdapter(adapter);
+        spinner_operacoes = (Spinner) findViewById(R.id.SpinnerOperacoes);
 
-        Operacao.setOnItemSelectedListener(this);
+        spinner_materias = (Spinner) findViewById(R.id.SpinnerMaterias_calculos);
+        ArrayAdapter<CharSequence> adapter_materias = ArrayAdapter.createFromResource(this, R.array.array_materias, android.R.layout.simple_spinner_item);
+        adapter_materias.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_materias.setAdapter(adapter_materias);
+        spinner_materias.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String operacaoSelecionada = spinner_materias.getSelectedItem().toString();
+                switch (operacaoSelecionada) {
+                    case "Física":
+                        ArrayAdapter<CharSequence> adapter_fisica = ArrayAdapter.createFromResource(InicioCalculadora.this, R.array.array_fisica, android.R.layout.simple_spinner_item);
+                        adapter_fisica.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        spinner_operacoes.setAdapter(adapter_fisica);
+                        break;
+                    case "Matemática":
+                        ArrayAdapter<CharSequence> adapter_matematica = ArrayAdapter.createFromResource(InicioCalculadora.this, R.array.array_matematica, android.R.layout.simple_spinner_item);
+                        adapter_matematica.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        spinner_operacoes.setAdapter(adapter_matematica);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinner_operacoes.setOnItemSelectedListener(this);
 
     }
 
-    //Verifica se um item do spinner foi selecionado e executa uma ação
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String operacaoSelecionada = Operacao.getSelectedItem().toString();
+        String operacaoSelecionada = spinner_operacoes.getSelectedItem().toString();
         switch (operacaoSelecionada) {
             case "Equação do 2º grau":
                 getSupportFragmentManager()
                         .beginTransaction()
                         .replace(R.id.container_calculadora, new Bhaskara()).commit();
+
                 break;
             case "Regra de três simples":
                 getSupportFragmentManager()
                         .beginTransaction()
                         .replace(R.id.container_calculadora, new RegraDeTres()).commit();
+
                 break;
             default:
                 break;
@@ -65,6 +91,8 @@ public class InicioCalculadora extends AppCompatActivity implements AdapterView.
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+
 
     // Faz com que o botão voltar da toolbar funcione igual ao do celular
     @Override
